dc48c1b27b7ba8ea8725ddc7f385c0da8ee6306f
Emanuel <emanuelzsmj@gmail.com>
2022-06-21 16:05:54 -0300

    Exercicio 01 Bootcamp

---
+++ Js1/ex1.js
@@ -0,0 +1,8 @@
+//Comentario de uma linha
+/* 
+Comentario de multiplas linhas
+*/
+let name = prompt('Digite seu nome: ')
+let age = prompt('Digite sua idade: ')
+let confirmAge = confirm('Voce realmente tem ' + age + ' anos, meliante?')
+alert('nome :' + name + '\nIdade: ' + age + '\nIdade Confirmada: ' + confirmAge)
