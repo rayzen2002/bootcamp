c0592108a92ce249e9a059b479874b328023a8d9
Emanuel <emanuelzsmj@gmail.com>
2022-06-22 14:39:23 -0300

    Exercicio 07

---
+++ Js1/ex7.js
@@ -0,0 +1,21 @@
+let spaceship = 'Elemental'
+
+switch (spaceship) {
+  case 'Golias':
+    console.log('Nave mais resistente')
+    break
+  case 'Elemental':
+    console.log('Nave com melhor armazenamento')
+    break
+  case 'Helmet':
+    console.log('Rapida ativacao de escudo')
+    break
+  case 'Frontier':
+    console.log('Nave de infantaria')
+    break
+  case 'Enterprise':
+    console.log('Nave da frota estelar')
+    break
+  default:
+    console.log('Descohecida')
+}
