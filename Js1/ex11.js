f9bc02dbd306bae6e26e3dbafe5573dbd037cef0
Emanuel <emanuelzsmj@gmail.com>
2022-06-27 12:23:06 -0300

    Exercicios 11 e 12

---
+++ Js1/ex11.js
@@ -0,0 +1,7 @@
+let velocity = 50
+let aceleration = 5
+
+do {
+  velocity += aceleration
+  console.log('Acelerando, estamos a: ' + velocity)
+} while (velocity < 100)
---
+++ Js1/ex12.js
@@ -0,0 +1,9 @@
+let spaceshipName = 'Supernova'
+
+for (let i = 0; i < spaceshipName.length; i++) {
+  if (spaceshipName[i] == 'o') {
+    break
+  } else {
+    console.log(spaceshipName[i])
+  }
+}
